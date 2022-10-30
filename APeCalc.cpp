6b99a12a443f1a53ba3c3a652ef1fceb3f188bed
Rodrigo Liberato <108103437+Rodrigllb@users.noreply.github.com>
2022-10-30 14:57:44 -0300

    Update APeCalc.cpp

--- APeCalc.cpp
+++ APeCalc.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 using namespace std;
 
-double perimetrocalc(double, double); //protótipos com argumentos
-void perimetro(void); // protótipos
-void perimetrocalcIncomum();
-void area(void);
-double areacalcP(double, double);
+//Calculando os perimetros regulares.
+double Calculando_Perimetro_Regular(double, double); 
+void Inicio_Perimetro(); 
+
+// Calculando a area, protótipos.
+void Inicio_area();
+double Calculo_Da_Area(double, double);
 
 //Perimetro de figuras irregulares, protótipos;
+void Perimetro_Calculo_Irregular();
 double TriânguloIrregular(double, double, double);
 double QuadradoIrregular(double, double, double, double);
 double PentágonoIrregular(double, double, double, double, double);
 
-int main(void)
+int main()
 {
 	system("chcp 1252 > nul");
 	system("cls");
@@ -21,129 +24,69 @@ int main(void)
 	cout << "project developed by rodrigo.\n";
 	cout << "**************************************\n\n";
 
-	int option; // váriavel inteira para armazenar um valor específico
-	cout << "Calcule: \n";
-	cout << "1. Perimetro de figura plana.\n";
-	cout << "2. Área de figura plana.\n";
-	cin >> option;
+	int OpçãoDeEscolhaInicial; // váriavel inteira para armazenar um valor específico
+	cout << "Calcule: \n" << "1. Perimetro de figura plana.\n" << "2. Área de figura plana.\n";
+	cin >> OpçãoDeEscolhaInicial;
 
-	switch (option) // ramificações, semelhante a condições!
+	switch (OpçãoDeEscolhaInicial) // ramificações, semelhante a condições!
 	{
 	case 1:
 		system("cls");
-		perimetro(); // chamada de funções.
+		Inicio_Perimetro(); // chamada de funções.
+		break;
 	case 2:
 		system("cls");
-		area(); //chamada da função area para o respectivo calculo
-
-	default:
-		cout << "Erro de comando\n"; //Isso assemelha-se com um laço de repetição, pois ao dar um comando intero errado, irá voltar novamente ao "main".
-		main(); //Chamada da função main.
-
-	}
-}
-void perimetro(void)
-{
-	double ladosDaFigura;
-	double valorDosLados;
-	system("cls"); // Limpeza de texto do prompt
-	
-	cout << "Perimetro de figura plana(Regulares e irregulares)\n";
-	int OpçãoDeLadosIncomuns;
-	cout << "Os valores variam na determinada figura plana?\n";
-	cout << "1. Sim\n" << "2. Não\n";
-	cin >> OpçãoDeLadosIncomuns;
-
-	switch (OpçãoDeLadosIncomuns)
-	{
-	case 1:
-		perimetrocalcIncomum();
-	case 2:
-		cout << "Continuando..\n";
+		Inicio_area(); //chamada da função area para o respectivo calculo
 		break;
 	default:
-		cout << "\n";
-		perimetro();
+		cout << "Erro de comando\n";
+		break;
+
 	}
+}
+void Inicio_Perimetro()
+{
+	double LadosDaFigura, ValorDosLados;
+	system("cls"); // Limpeza de texto do prompt
+	int OpçãoDeLadosIrregulares;
+	
+	cout << "Perimetro de figura plana(Regulares e irregulares)\n" 
+		 << "Você deseja calcular figuras regulares ou irregulares?\n" 
+		 << "1. Figuras Regulares\n" << "2. Figuras Irregulares\n";
+	cin >> OpçãoDeLadosIrregulares;
 
-	cout << "Perimetro de figura plana (Polignos regulares)\n";
-	cout << "Quantos lados a sua figura?\n";
-	cin >> ladosDaFigura;
-	cout << "Valor dos lados\n";
-	cin >> valorDosLados;
-
-	cout << "Resultado do perimetro será: " << perimetrocalc(ladosDaFigura, valorDosLados); //Exibe o valor de retorno, como resultado pós calculo.
-	cout << endl;
-
-	//****Condições dos valores dos lados de uma figura comum, sem variação de valores atribuidos***//
-	if (ladosDaFigura == 3)
-		cout << "Tipo de figura: triângulo\n"; // Condição que apresenta o nome da figura após o valor especificado pelo usuário.
-	if (ladosDaFigura == 4)
-		cout << "Tipo de figura: quadrado\n"; //Além do mais, ainda tenho muitas dúvidas acerca do "if" e o "switch" kk, será que o switch é melhor nessas situações?
-	if (ladosDaFigura == 5)
-		cout << "Tipo de figura: pentágono\n";
-	if (ladosDaFigura == 6)
-		cout << "Tipo de figura: hexágono\n";
-	if (ladosDaFigura == 7)
-		cout << "Tipo de figura: heptágono\n";
-	if (ladosDaFigura == 8)
-		cout << "Tipo de figura: octógono\n";
-	if (ladosDaFigura == 9)
-		cout << "Tipo de figura: eneágono\n";
-	if (ladosDaFigura == 10)
-		cout << "Tipo de figura: decágono\n";
-	if (ladosDaFigura == 11)
-		cout << "Tipo de figura: undecágono\n";
-	if (ladosDaFigura == 12)
-		cout << "Tipo de figura: dodecágono\n";
-	if (ladosDaFigura == 13)
-		cout << "Tipo de figura: tridecágono\n";
-	if (ladosDaFigura == 14)
-		cout << "Tipo de figura: tetradecágono\n";
-	if (ladosDaFigura == 15)
-		cout << "Tipo de figura: pentadecágono\n";
-	if (ladosDaFigura == 16)
-		cout << "Tipo de figura: hexadecágono\n";
-	if (ladosDaFigura == 17)
-		cout << "Tipo de figura: heptadecágono\n";
-	if (ladosDaFigura == 18)
-		cout << "Tipo de figura: octadecágono\n";
-	if (ladosDaFigura == 19)
-		cout << "Tipo de figura: eneadecágono\n";
-	if (ladosDaFigura == 20)
-		cout << "Tipo de figura: icoságono\n";
-	if (ladosDaFigura > 20)
-		cout << "É sério que existe poligno com até mil lados!!??\n";
-
-	cout << endl;
-
-	int OpçãoDeLadosComuns;
-	cout << "Deseja realizar a operação novamente?\n" << "1. Sim\n" << "2. Não\n";
-	cin >> OpçãoDeLadosComuns;
-	switch (OpçãoDeLadosComuns) // Ramificações, semelhante ao 'if'.
+	switch (OpçãoDeLadosIrregulares)
 	{
 	case 1:
-		perimetro();
+		cout << "Continuando..\n";
+		break;
 	case 2:
-		cout << "voltando ao menu anterior..\n";
-		system("pause");
-		main();
+		Perimetro_Calculo_Irregular();
+		break;
 	default:
 		cout << "erro de comando\n";
-		system("cls");
-		perimetro();
-
+		break;
 	}
 
+	cout << "Perimetro de figura plana (Figuras regulares)\n" << "Quantos lados têm a sua figura?\n";
+	cin >> LadosDaFigura;
+	cout << "Valor dos lados\n";
+	cin >> ValorDosLados;
+
+	cout << "Resultado do perimetro será: " << Calculando_Perimetro_Regular(LadosDaFigura, ValorDosLados); //Exibe o valor de retorno, como resultado pós calculo.
+	cout << endl;
+
+	int Opção_DeLadosRegulares;
+	cout << "Deseja realizar a operação novamente?\n" << "1. Sim\n" << "2. Não\n";
+	cin >> Opção_DeLadosRegulares;
+
 }
-double perimetrocalc(double QuantidadeDelados, double ladosComValor)
+double Calculando_Perimetro_Regular(double QuantidadeDelados, double LadosComValor)
 {
-	double resultadoPerimetro;
-	return resultadoPerimetro = QuantidadeDelados * ladosComValor; //Calculando o perimetro e retornando o valor;
-
-
+	double Resultado_Do_Perimetro;
+	return Resultado_Do_Perimetro = QuantidadeDelados * LadosComValor; //Calculando o perimetro e retornando o valor;
 }
-void perimetrocalcIncomum()
+void Perimetro_Calculo_Irregular()
 {
 	system("cls");
 	cout << "Calculo de figuras planas (Polignos irregulares).\n";
@@ -171,11 +114,15 @@ void perimetrocalcIncomum()
 		cout << "1. Sim 2.Não, desejo retornar ao menu inicial.\n";
 		cin >> OpçãoDeEscolha;
 
+
 		if (OpçãoDeEscolha == 1)
-			perimetrocalcIncomum();
-		if (OpçãoDeEscolha == 2)
+		{
+			Perimetro_Calculo_Irregular();
+		}
+		else if (OpçãoDeEscolha == 2)
+		{
 			main();
-		
+		}
 
 	case 4:
 		cout << "Ok, um quadrado irregular. Quais são os valores dos lados?\n";
@@ -197,10 +144,12 @@ void perimetrocalcIncomum()
 		cin >> OpçãoDeEscolha;
 
 		if (OpçãoDeEscolha == 1)
-			perimetrocalcIncomum();
-		if (OpçãoDeEscolha == 2)
+		{
+			Perimetro_Calculo_Irregular();
+		} else if (OpçãoDeEscolha == 2)
+		{
 			main();
-
+		}
 
 	case 5:
 		cout << "Ok, um pentágono irregular. Quais são os valores dos lados?\n";
@@ -223,26 +172,34 @@ void perimetrocalcIncomum()
 		cout << "1. Sim 2.Não, desejo retornar ao menu inicial.\n";
 		cin >> OpçãoDeEscolha;
 
+
 		if (OpçãoDeEscolha == 1)
-			perimetrocalcIncomum();
-		if(OpçãoDeEscolha == 2)
+		{
+			Perimetro_Calculo_Irregular();
+		}
+		else if (OpçãoDeEscolha == 2)
+		{
 			main();
+		}
 
 	default:
-		cout << "(1 e 2 lados são inexistentes) Error, no momento só é suportado até um poligono de 5 lados, tente novamente.\n";
+		cout << "Erro, no momento só é suportado até um poligono de 5 lados, tente novamente.\n";
 		system("pause > nul"); //Pausa para que o usuário leia 
-		perimetrocalcIncomum();
+		Perimetro_Calculo_Irregular();
 	}
+	
 
 }
 double TriânguloIrregular(double lado1, double lado2, double lado3) //Calculo do triângulo irregular.
 {
 	double ResultadoDoPerimetroIrregular_Triangular; //Nomeclatura redundante para melhor compreensão.
+
 	return ResultadoDoPerimetroIrregular_Triangular = lado1 + lado2 + lado3;
 }
 double QuadradoIrregular(double lado1, double lado2, double lado3, double lado4) //Calculo do quadrado irregular.
 {
 	double ResultadoDoPerimetroIrregular_Quadrancular;  //Nomeclatura redundante para melhor compreensão.
+
 	return ResultadoDoPerimetroIrregular_Quadrancular = lado1 + lado2 + lado3 + lado4;
 }
 double PentágonoIrregular(double lado1, double lado2, double lado3, double lado4, double lado5) //Calculo do pentágono irregular.
@@ -250,16 +207,16 @@ double PentágonoIrregular(double lado1, double lado2, double lado3, double lado
 	double ResultadoDoPerimetroIrregular_Pentagonal;  //Nomeclatura redundante para melhor compreensão.
 	return ResultadoDoPerimetroIrregular_Pentagonal = lado1 + lado2 + lado3 + lado4 + lado5;
 }
-void area(void)
+void Inicio_area()
 {
 	system("cls");
 	cout << "Área de figuras planas\n";
-	int opção;
+	int Opção_Para_Escolha;
 	cout << "Qual figura deseja escolher para iniciar o calculo?\n"; //Inicialmente o calculo se iniciará pelo triângulo.
 	cout << "1. Triângulo\n" << "2. Sair\n";
-	cin >> opção;
+	cin >> Opção_Para_Escolha;
 
-	switch (opção)
+	switch (Opção_Para_Escolha)
 	{
 	case 1:
 		cout << endl;
@@ -269,13 +226,13 @@ void area(void)
 		cout << "Qual a altura?\n";
 		cin >> altura;
 
-		areacalcP(base, altura);
-		cout << "O resultado da área é: " << areacalcP(base, altura) << endl << endl;
+		Calculo_Da_Area(base, altura);
+		cout << "O resultado da área é: " << Calculo_Da_Area(base, altura) << endl << endl;
 		system("pause > nul");
 		cout << "Reiniciando...\n";
 		system("pause > nul");
 
-		area();
+		Inicio_area();
 
 	case 2:
 		system("cls");
@@ -284,11 +241,11 @@ void area(void)
 
 	default:
 		cout << "erro de comando..\n";
-		area();
+		Inicio_area();
 	}
 }
-double areacalcP(double valorDaBase, double valorDaAltura)
+double Calculo_Da_Area(double ValorDaBase, double ValorDaAltura)
 {
 	double AreaResultado;
-	return AreaResultado = valorDaBase * valorDaAltura / 2; // a = b*h/2 Calculando a area de um triangulo.
+	return AreaResultado = ValorDaBase * ValorDaAltura / 2; // a = b*h/2 Calculando a area de um triangulo.
 }
